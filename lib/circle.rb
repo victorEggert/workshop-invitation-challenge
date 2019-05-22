7ab767727ad436e1c3b85eb6a4bac5f01731ee88
victorhe <victorheggert@gmail.com>
2019-05-22 19:06:39 -0300

    DONE

--- lib/circle.rb
+++ lib/circle.rb
@@ -4,11 +4,14 @@ class Circle < GeometricBase
   attr_writer :radius
 
   def initialize(radius)
+    @radius = radius
   end
 
   def area
+    multiply(PI, exponentiation(@radius, 2))
   end
 
   def perimeter
+    multiply(multiply(PI, @radius), 2)
   end
 end
